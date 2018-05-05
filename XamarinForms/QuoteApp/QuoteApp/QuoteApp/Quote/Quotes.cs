c6ba3ccc296780da524e6dc4a35c11bedd126fb6
kybanez12 <kybanez12@gmail.com>
2018-05-05 20:29:44 +1000

    commiting app for submission

---
+++ XamarinForms/QuoteApp/QuoteApp/QuoteApp/Quote/Quotes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuoteApp.Quote
+{
+    public class Quotes
+    {
+        public string[] quotes = new string[3];
+        public string newQuote { get; set; }
+        public string oldQuote { get; set; }
+
+        public Quotes()
+        {
+            quotes[0] = "you suck";
+            quotes[1] = "fake it till you make it";
+            quotes[2] = "go fast and commit";
+        }
+
+        public void GenerateQuote()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, quotes.Length);
+            newQuote = quotes[randomNumber];
+        }
+    }
+}
---
+++ XamarinForms/QuoteApp/QuoteApp/QuoteApp/QuotePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using QuoteApp.Quote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QuoteApp
+{
+
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class QuotePage : ContentPage
+	{
+        Quotes quotes;
+
+        public QuotePage ()
+		{
+            InitializeComponent();
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            quotes = new Quotes();
+            quotes.GenerateQuote();
+            BindingContext = quotes;
+        }
+    }
+
+}
